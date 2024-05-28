424d9a5161824c6605441b6cb4cb8f6b7555746d
Megabyte94 <1011032007KBz*>
2024-05-28 10:20:37 -0500

    Chapter 12: Firebase Auth user creation and connfiguration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
+// App entry point
 void main() {
+  // Ensure that the Flutter bindings are initialized.
+  WidgetsFlutterBinding.ensureInitialized();
+  // Run the app
   runApp(const MyApp());
 }
 
@@ -11,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Notes',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -21,11 +28,89 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+//1----------
+
+// Home page
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // Initialize text editing controllers
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  // It creates instances of TextEditingController and assigns them to the _email and _password variables.
+  @override
+  void initState() {
+    _email = TextEditingController();
+    _password = TextEditingController();
+    super.initState();
+  }
+
+  /* This code disposes the text editing controllers when the widget is removed from the widget tree.
+  It calls the dispose() method on the _email and _password controllers to release any resources they hold.*/
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
+  }
+
+  // This method builds the widget tree for the HomePage widget.
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    // Create a Scaffold widget with an AppBar and a body.
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Register"),
+        ),
+        body: FutureBuilder(
+          future: Firebase.initializeApp(
+            options: DefaultFirebaseOptions.currentPlatform,
+          ),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                return const Text('None');
+              case ConnectionState.waiting:
+                return const CircularProgressIndicator();
+              case ConnectionState.active:
+                return const Text('Active');
+              case ConnectionState.done:
+                return Column(
+                  children: [
+                    TextField(
+                      controller: _email,
+                      decoration: const InputDecoration(labelText: 'Email'),
+                    ),
+                    TextField(
+                      controller: _password,
+                      decoration: const InputDecoration(labelText: 'Password'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        try {
+                          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                            email: _email.text,
+                            password: _password.text,
+                          );
+                        } on FirebaseAuthException catch (e) {
+                          print('Failed with error code: ${e.code}');
+                          print(e.message);
+                        }
+                      },
+                      child: const Text('Register'),
+                    ),
+                  ],
+                );
+              default:
+                return const Text('Error initializing Firebase');
+            }
+          },
+        ));
   }
 }
